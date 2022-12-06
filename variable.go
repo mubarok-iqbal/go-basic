a28f80a942b2f9e07b78e7fa655cf7250e9494ed
muhammad iqbal Mubarok <mubarok.iqbal@gmail.com>
2022-12-06 20:50:50 +0700

    go variable

---
+++ variable.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Muhammad Iqbal Mubarok"
+	fmt.Println(name)
+
+	var age int8 = 12
+	fmt.Println(age)
+
+	var city = "Bandung"
+	fmt.Println(city)
+}
