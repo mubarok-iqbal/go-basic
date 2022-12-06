1a8f4560947f14c31a54bf2ea0b91a437916949f
muhammad iqbal Mubarok <mubarok.iqbal@gmail.com>
2022-12-06 21:01:49 +0700

    go constant

---
+++ constant.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	const A int = 20
+	const B = "test"
+	const (
+		firstName = "Muhammad Iqbal"
+		lastName  = "Mubarok"
+	)
+
+	fmt.Println(firstName, lastName)
+}
