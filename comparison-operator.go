2abe0be55dfc780212b53aed2f15e7ca55b42203
muhammad iqbal Mubarok <mubarok.iqbal@gmail.com>
2022-12-08 09:03:23 +0700

    comparison operator

---
+++ comparison-operator.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	var firstName = "Muhammad Iqbal"
+	var lastName = "Mubarok"
+
+	var result = firstName == lastName
+	var result2 = firstName != lastName
+
+	fmt.Println(result, result2)
+
+	var a = 2
+	var b = 3
+	fmt.Println(a < b)
+	fmt.Println(a > b)
+	fmt.Println(a == b)
+	fmt.Println(a == b)
+	fmt.Println(a != b)
+	fmt.Println(a <= b)
+	fmt.Println(a >= b)
+
+}
